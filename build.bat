4f5bb92a883c9a1549fa85d44dfa6dd92caf855c
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2015-06-25 15:13:32 +1000

    Delete build.bat

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-C:\Users\Luke\AppData\Local\nasm\nasm.exe -f bin "C:\Users\Luke\Programming\My OS\Stage1\Boot.asm" -o "C:\Users\Luke\Programming\My OS\Release\Boot.bin"
-C:\Users\Luke\AppData\Local\nasm\nasm.exe -f bin "C:\Users\Luke\Programming\My OS\Stage2\Stage2.asm" -o "C:\Users\Luke\Programming\My OS\Release\Stage2.bin"
-C:\Users\Luke\AppData\Local\nasm\nasm.exe -f bin "C:\Users\Luke\Programming\My OS\Kernel\Kernel.asm" -o "C:\Users\Luke\Programming\My OS\Release\Kernel.bin"
-
-copy "C:\Users\Luke\Programming\My OS\Release\Stage2.bin" "A:\Stage2.bin"
-copy "C:\Users\Luke\Programming\My OS\Release\Kernel.bin" "A:\Kernel.bin"
-
-pause
